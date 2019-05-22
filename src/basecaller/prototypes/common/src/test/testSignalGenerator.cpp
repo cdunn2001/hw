930b710618411b7fca1ed249108fccf1e22d7108
Ben Byington <bbyington@pacificbiosciences.com>
2019-05-22 11:34:44 -0400

    BEN-600 Fixed bug in testSignalGenerator
    A recent merge accidentally broke this test, as it relied on being able
    to load a specified subset of the trace file, and that functionality
    was removed.  This commit updates the rest of the test to no longer
    rely on that

--- src/basecaller/prototypes/common/src/test/testSignalGenerator.cpp
+++ src/basecaller/prototypes/common/src/test/testSignalGenerator.cpp
@@ -6,8 +6,6 @@
 #include <common/ZmwDataManager.h>
 #include <common/DataGenerators/SignalGenerator.h>
 #include <pacbio/primary/SequelTraceFile.h>
-#include <pacbio/primary/SequelDefinitions.h>
-#include <pacbio/primary/Tile.h>
 
 using namespace PacBio::Cuda;
 using namespace PacBio::Cuda::Data;
@@ -34,7 +32,6 @@ TEST(SignalGeneratorTest, CompareData)
     using PacBio::Primary::zmwsPerTranche;
 
     static constexpr size_t zmwLaneWidth = 64;
-    static constexpr size_t numFrames = 512;
 
     auto dataParams = DataManagerParams()
             .NumBlocks(4)
@@ -46,25 +43,6 @@ TEST(SignalGeneratorTest, CompareData)
     auto traceParams = TraceFileParams()
             .TraceFileName("/pbi/dept/primary/sim/spider/designer_spider1p0NTO_fv2p4_SNR-50.trc.h5");
 
-    std::vector<int16_t> zmwsByFrame(zmwLaneWidth * numFrames);
-    {
-        SequelTraceFileHDF5 traceIn(traceParams.traceFileName);
-        for (size_t zmwOffset = 0; zmwOffset < zmwLaneWidth; zmwOffset += zmwsPerTranche)
-        {
-            std::vector<int16_t> data(zmwsPerTranche * Tile::NumPixels * numFrames);
-            size_t numFramesRead = traceIn.ReadLaneSegment(zmwOffset, 0, numFrames, data.data());
-            EXPECT_EQ(numFrames, numFramesRead);
-            for (size_t frame = 0; frame < numFrames; frame++)
-            {
-                for (size_t zmw = 0; zmw < zmwsPerTranche; zmw++)
-                {
-                    zmwsByFrame[((zmwOffset + zmw) * 512) + frame] = data[(frame * Tile::NumPixels) + zmw];
-                }
-            }
-        }
-    }
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
     ZmwDataManager<int16_t> manager(dataParams,
                                     std::make_unique<SignalGenerator>(dataParams, traceParams),
                                     true);
@@ -72,7 +50,10 @@ TEST(SignalGeneratorTest, CompareData)
     static constexpr size_t numLanesToCheck = 16;
     std::random_device rd;
     std::mt19937 eng(rd());
-    std::uniform_int_distribution<> distr(0, dataParams.laneWidth);
+    std::uniform_int_distribution<> distr(0, dataParams.kernelLanes);
+
+    SequelTraceFileHDF5 traceIn(traceParams.traceFileName);
+    std::vector<int16_t> truth(dataParams.laneWidth * dataParams.blockLength);
 
     while (manager.MoreData())
     {
@@ -87,13 +68,16 @@ TEST(SignalGeneratorTest, CompareData)
             auto blockView = in.GetBlockView(lane);
             std::vector<int16_t> expectedPixelSums(zmwLaneWidth, 0);
             std::vector<int16_t> pixelSums(zmwLaneWidth, 0);
+
+            const size_t zmwOffset = batchIdx * dataParams.laneWidth * dataParams.kernelLanes + lane * dataParams.laneWidth;
+            traceIn.Read1CZmwLaneSegment(zmwOffset, dataParams.laneWidth, frameOffset, dataParams.blockLength, truth.data());
             for (size_t frame = 0; frame < dataParams.blockLength; ++frame)
             {
                 auto rowOffset = frame * dataParams.laneWidth;
                 for (size_t k = 0; k < dataParams.laneWidth; ++k)
                 {
-                    pixelSums[k] += blockView[rowOffset + k];
-                    expectedPixelSums[k] += zmwsByFrame[k * numFrames + (frameOffset + frame)];
+                    pixelSums[k] += blockView(frame, k);
+                    expectedPixelSums[k] += truth[rowOffset + k];
                 }
             }
 
