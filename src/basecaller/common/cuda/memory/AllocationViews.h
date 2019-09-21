4e5d068bf6a6e40d27d7a9da3f62d446efb0f2e2
Curtis Gehman <cgehman@pacificbiosciences.com>
2019-09-20 17:45:37 -0700

    BEN-738: Add `begin` and `end` iterator methods to `HostView`.

--- src/basecaller/common/cuda/memory/AllocationViews.h
+++ src/basecaller/common/cuda/memory/AllocationViews.h
@@ -56,7 +56,18 @@ public:
 
     T* Data() { return data_; }
     const T* Data() const { return data_; }
+
     size_t Size() const { return len_; }
+
+    const T* begin() const  { return data_; }
+    const T* end() const  {return data_ + len_; }
+
+    const T* cbegin() const  { begin(); }
+    const T* cend() const { end(); }
+
+    T* begin() { return data_; }
+    T* end() { return data_ + len_; }
+
 private:
     T* data_;
     size_t len_;
