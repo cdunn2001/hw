7bcaf7966562f102d1a19309e58cdaa005c3bb00
Ben Byington <bbyington@pacificbiosciences.com>
2019-10-10 18:37:27 -0400

    BEN-1141 Added workaround for tbb in mongo
    Our build system currently has a problem where cmake
    points to one version of tbb, but the intel compiler
    polutes our environment and causes us to actually pull
    tbb headers from a different version of tbb.  For the part
    of the API used on sequel the difference didn't matter,
    but for the tbb graphs there is an ABI difference that
    prevents compilation.  For now mongo will use both headers
    and libraries from the compiler installation, though hopefully
    that will be fixed in the future

--- src/basecaller/applications/test/run_cram.sh
+++ src/basecaller/applications/test/run_cram.sh
@@ -9,6 +9,8 @@ if [[ ! -v NO_MODULE_LOAD ]]; then
     module use /pbi/dept/primary/modulefiles
     module use /mnt/software/modulefiles
     module purge
+    # temporarily required for running mongo, until some tbb issues get resolved
+    module load composer_xe/2017.4.196
     module load cram/0.7
     module load primary-toolkit/1.0.7
     unset WORKSPACE  # if this is set, then the following module will use it instead of the cwd
