bfc0cd30f476f90084f6d41801157a46800b4ebb
Ben Byington <bbyington@pacificbiosciences.com>
2019-12-02 17:13:20 -0500

    BEN-1294 minor cleanup

--- src/basecaller/local_setup.sh
+++ src/basecaller/local_setup.sh
@@ -1,12 +1,12 @@
 # Caution:
 # This is only intended as a convenience tool if for whatever reason a developer needs to make
-# a change affecting both this repository and pa-third-common.  This should be an uncommon need,
-# and if you find yourself needing it, please evaluate your situation to make sure it's truly
-# necessary and appropriate.
+# a change affecting both this repository and pa-third-party/common.  This should be an uncommon
+# need, and if you find yourself needing it often, please evaluate your situation to make sure
+# it's truly necessary and appropriate.
 #
 # Please do not change the names/locations of submodules.  This repository has a .gitignore set
 # up to keep any submodule usage local to the develop and not part of the centralized repository.
-# If any naming changes then any submodule usage will likely get pushed up to the remote.
+# If any naming changes then any submodule usage may accidentally get pushed up to the remote.
 #
 # Beware: If a local version is needed, it is entirely the responsibility of the developer to
 #         make sure they are building with consistent versions.  The develop branch is checked
