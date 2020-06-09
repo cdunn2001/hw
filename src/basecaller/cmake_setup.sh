1adaad8c4ac4a72cf46316f362df6478757712fc
Ben Byington <bbyington@pacificbiosciences.com>
2020-06-09 15:32:50 -0400

    SEQ-6626 Updated build layout to be more ninja friendly

--- src/basecaller/cmake_setup.sh
+++ src/basecaller/cmake_setup.sh
@@ -1,4 +1,5 @@
 distclean=${distclean:-0}  # set to 1 if you want to obliterate all previous build artifacts
+generator=${generator:-Unix Makefiles}
 
 if [[ $distclean != 0 ]]
 then
@@ -19,8 +20,8 @@ mkdir -p build/x86_64/Release
 
 # Note: CMAKE_CUDA_HOST_COMPILER must be set here.  By the time we're in a cmake script (e.g. when parsing the toolchain file) it's already too late
 #       and cuda will have at least partially latched on to whatever random host compiler it managed to find
-(cd build/x86_64/Debug_gcc;       cmake --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_TOOLCHAIN_FILE=TC-gcc-cuda-x86_64.cmake ../../..)
-(cd build/x86_64/Release_gcc;     cmake --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=gcc -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=TC-gcc-cuda-x86_64.cmake ../../..)
-(cd build/x86_64/Debug;           cmake --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=icpc -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_TOOLCHAIN_FILE=TC-icc-cuda-x86_64.cmake ../../..)
-(cd build/x86_64/Release;         cmake --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=TC-icc-cuda-x86_64.cmake ../../..)
+(cd build/x86_64/Debug_gcc;       cmake "-G${generator}" --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_TOOLCHAIN_FILE=TC-gcc-cuda-x86_64.cmake ../../..)
+(cd build/x86_64/Release_gcc;     cmake "-G${generator}" --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=gcc -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=TC-gcc-cuda-x86_64.cmake ../../..)
+(cd build/x86_64/Debug;           cmake "-G${generator}" --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=icpc -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_TOOLCHAIN_FILE=TC-icc-cuda-x86_64.cmake ../../..)
+(cd build/x86_64/Release;         cmake "-G${generator}" --debug-trycompile -DCMAKE_CUDA_HOST_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=TC-icc-cuda-x86_64.cmake ../../..)
 
