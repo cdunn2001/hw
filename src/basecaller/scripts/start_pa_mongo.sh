72e1d11f700ded2b6696563adb04c44bac56f848
Mark Lakata <mlakata@pacificbiosciences.com>
2020-11-17 17:05:23 -0800

    added script to run smrt-basecaller using WX2

--- src/basecaller/scripts/start_pa_mongo.sh
+++ src/basecaller/scripts/start_pa_mongo.sh
@@ -6,6 +6,8 @@
 
 BUILD=${BUILD:-Release_gcc}
 LOGFILTER=${LOGFILTER:-info}
+FRAMES=${FRAMES:-1024}
+RATE=${RATE:-100}
 
 if [[ $GDBSERVER ]]
 then 
@@ -19,5 +21,33 @@ else
     cmd=""
 fi
 
+rows=2756
+cols=2912
+zmws=$(( rows * cols ))
+lanesPerPool=$(( zmws / 64 ))
+
+#      "lanesPerPool": $lanesPerPool,
+#      "zmwsPerLane": 64
+cat <<HERE > /tmp/config.json
+{
+  "layout" :
+  {
+      "framesPerChunk": 512
+  },
+  "source":
+  {
+    "sourceType": "WX2",
+    "wx2SourceConfig":
+    {
+         "dataPath": "HardLoop",
+         "platform": "Spider",
+         "simulatedFrameRate": $RATE
+    }
+  }
+} 
+HERE
+
+cat /tmp/config.json
+
 cd ../build/x86_64/${BUILD}
-sudo $cmd ./applications/smrt-basecaller --config source.sourceType=WX2 --outputbazfile=/data/pa/bogus.baz --frames=1024 --inputfile=constant --config layout.framesPerChunk=512 --config source.wx2SourceConfig.dataPath=HardLoop --config source.wx2SourceConfig.platform=Spider --logfiler=${LOGFILTER}
+sudo $cmd ./applications/smrt-basecaller --outputbazfile=/data/pa/bogus.baz --frames=${FRAMES} --inputfile=constant/123 --logfilter=${LOGFILTER} --config /tmp/config.json --nop
