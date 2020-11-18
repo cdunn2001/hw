faeb8e2f8456b654c34897e40a098cacdd564e42
Mark Lakata <mlakata@pacificbiosciences.com>
2020-11-17 17:04:52 -0800

    local openht build is needed to build a successful smrt-basecaller

--- src/basecaller/module_setup.sh
+++ src/basecaller/module_setup.sh
@@ -6,13 +6,15 @@ module purge
 module use /pbi/dept/primary/modulefiles
 module use /mnt/software/modulefiles
 module load devtoolset/6
-module load convey/OpenHT/2.1.35
+# module load convey/OpenHT/2.1.35
+module load openht/git
 module load composer_xe/2017.4.196
 module load pacbio-devtools
 module load patchelf
 module load libusb/1.0.22
 module load cmake/3.13.3
 module load cuda/10.1.168_418.67
+module load ninja/1.10.0
 
 # to build the Wolverine Personality:
 # module load vivado/2018.3
