6f39791d0a4d4c429407685d905e500b443c89f9
Mark Lakata <mlakata@pacificbiosciences.com>
2021-01-24 22:21:36 -0800

    work in progress with the launcher script for testing loopback

--- src/basecaller/scripts/start_pa_mongo.sh
+++ src/basecaller/scripts/start_pa_mongo.sh
@@ -8,13 +8,22 @@ BUILD=${BUILD:-Release_gcc}
 LOGFILTER=${LOGFILTER:-info}
 FRAMES=${FRAMES:-1024}
 RATE=${RATE:-100}
-NOP=${NOP:-0}
 TRACE_OUTPUT=${TRACE_OUTPUT:-/data/pa/test.trc.h5}
 INPUT=${INPUT:-alpha}
+VSC=${VSC:-1}
+NOP=${NOP:-0}
+
+
+
+INPUT=/pbi/dept/primary/sim/mongo/test_designer_mongo_acgt_SNR-40.trc.h5
+# --numZmwLanes 1 --config layout.lanesPerPool=1 --config algorithm.pulseAccumConfig.Method=HostSimulatedPulses --frames 1024
+
+
 
 if [[ $GDBSERVER ]]
 then 
-    cmd="/home/UNIXHOME/mlakata/clion-2020.1.1/bin/gdb/linux/bin/gdbserver localhost:2000"
+#    cmd="/home/UNIXHOME/mlakata/clion-2020.1.1/bin/gdb/linux/bin/gdbserver localhost:2000"
+    cmd="/home/UNIXHOME/mlakata/local/gdb-10.1/bin/gdbserver localhost:2000"
     BUILD=Debug_gcc
 elif [[ $GDB ]]
 then
@@ -26,34 +35,26 @@ fi
 
 if [[ $TRACE_OUTPUT != "" ]]
 then
-    if [[ $NOP == 1 ]]
-    then
-        echo "Can't use NOP and TRACE_OUTPUT together"
-        exit 1
+    if [[ $NOP == ! ]]
+    then  
+       echo "NOP=1 can't be used with TRACE_OUTPUT"
+       exit 1
     fi
     trc_output="--outputtrcfile $TRACE_OUTPUT"
 fi
-
-if [[ $NOP  == 1 ]]
-then
-    nopoption="--nop"
-else
-    nopoption=""
-fi
+nopoption="--nop=${NOP}"
 
 rows=2756
 cols=2912
-zmws=$(( rows * cols ))
-lanesPerPool=$(( zmws / 64 ))
+numZmws=$(( $rows * $cols ))
+numZmwLanes=$(( numZmws / 64 ))
 
-#      "lanesPerPool": $lanesPerPool,
-#      "zmwsPerLane": 64
 cat <<HERE > /tmp/config.json
 {
   "layout" :
   {
       "framesPerChunk": 512,
-      "lanesPerPool" : 1,
+      "lanesPerPool" : 8,
       "zmwsPerLane": 64
   },
   "source":
@@ -62,18 +63,26 @@ cat <<HERE > /tmp/config.json
     "wx2SourceConfig":
     {
          "dataPath": "HardLoop",
-         "platform": "Spider",
-         "simulatedFrameRate": $RATE
+         "platform": "Sequel2Lvl1",
+         "simulatedFrameRate": $RATE,
+         "sleepDebug": 600
     }
+  },
+  "traceROI": {
+    "roi": [ [ 0,0, 1, 64 ], [0,64,1,64], [2, 64, 1, 64] ]
   }
 } 
 HERE
 
-numZmwLanes=1
-
 cat /tmp/config.json
 
-cd ../build/x86_64/${BUILD}
+if [[ $VSC == 0 ]]
+then
+  cd ../build/x86_64/${BUILD}
+else
+  cd ../build
+fi
+
 # sudo $cmd ./applications/smrt-basecaller --outputbazfile=/data/pa/bogus.baz --frames=${FRAMES} --inputfile=constant/123 --logfilter=${LOGFILTER} --config /tmp/config.json ${nop_option} ${trc_output}
 set -x
 $cmd ./applications/smrt-basecaller --frames=${FRAMES} --numZmwLanes=${numZmwLanes}  --inputfile=${INPUT} --logfilter=${LOGFILTER} --config /tmp/config.json ${nop_option} ${trc_output}
