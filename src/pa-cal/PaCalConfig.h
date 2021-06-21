b14e4e25459e8cb81c9b3036ab0e6f9e43c396cc
Mark Lakata <mlakata@pacificbiosciences.com>
2021-06-21 16:46:51 -0700

    1. removed  SYSTEMD_COMMON_JSON from pa-cal because it will never be used 2. legal boilerplate added to PaCalConfig* and explicit constructors annocated 3. removed unnecessary comments from PaCalProcess::RunAllThreads

--- src/pa-cal/PaCalConfig.h
+++ src/pa-cal/PaCalConfig.h
@@ -1,3 +1,32 @@
+// Copyright (c) 2021, Pacific Biosciences of California, Inc.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE CONSTITUTES AND EMBODIES PACIFIC BIOSCIENCES' CONFIDENTIAL
+// AND PROPRIETARY INFORMATION.
+//
+// Disclosure, redistribution and use of this software is subject to the
+// terms and conditions of the applicable written agreement(s) between you
+// and Pacific Biosciences, where "you" refers to you or your company or
+// organization, as applicable.  Any other disclosure, redistribution or
+// use is prohibited.
+//
+// THIS SOFTWARE IS PROVIDED BY PACIFIC BIOSCIENCES AND ITS CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// File Description:
+///  \brief Defines the calibration parameters for the pa-cal application
+
+
 #ifndef KES_CAL_CONFIG_H
 #define KES_CAL_CONFIG_H
 
@@ -24,14 +53,14 @@ struct PaCalConfig : PacBio::Configuration::PBConfig<PaCalConfig>
 
     /// This is a convenient ctor for use in unit tests where one wants to hardcode the platform type
     /// using strict enum types, rather than feed in a JSON object.
-    PaCalConfig(PacBio::Sensor::Platform p) 
+    explicit PaCalConfig(PacBio::Sensor::Platform p) 
        : PaCalConfig( [p](){ 
            Json::Value x;
            x["platform"]=p.toString();
            return x;
          }() ) 
     {}
-    PaCalConfig(PacBio::Sensor::Platform::RawEnum p) 
+    explicit PaCalConfig(PacBio::Sensor::Platform::RawEnum p) 
         : PaCalConfig(PacBio::Sensor::Platform(p))
     {}  
 
