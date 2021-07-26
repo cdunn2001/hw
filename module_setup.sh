684235464caeeeb2dd1936db82ee41db62894d98
Ben Byington <bbyington@pacificbiosciences.com>
2021-07-26 08:57:30 -0700

    PTSD-567 Updated pa-ws build to use gcc8

--- module_setup.sh
+++ module_setup.sh
@@ -4,7 +4,7 @@ module purge
 
 module use /pbi/dept/primary/modulefiles
 module use /mnt/software/modulefiles
-module load devtoolset/6
+module load devtoolset/8
 module load composer_xe/2017.4.196
 module load pacbio-devtools
 module load patchelf
--- src/pa-ws/nexus-versions/Fetchzlib.cmake
+++ src/pa-ws/nexus-versions/Fetchzlib.cmake
@@ -1,4 +1,4 @@
 include(findpathirdparty)
 
-set(zlib_VERSION 1.2.8-bid3 CACHE INTERNAL "zlib version")
+set(zlib_VERSION gcc8-1.2.8-bid4 CACHE INTERNAL "zlib version")
 findpathirdparty(zlib zlib ${zlib_VERSION} ${CMAKE_SYSTEM_PROCESSOR})
