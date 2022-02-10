43fa20b7fd1131f6b120dd15c1ffda4f049855b6
Christopher Dunn <cdunn@pacificbiosciences.com>
2022-02-09 19:00:51 -0800

    Upgrade common and boost to match Basecaller in this repo
    This provides CurlPlusPlus::GetContentBundle().

--- src/pa-ws/nexus-versions/Fetchboost.cmake
+++ src/pa-ws/nexus-versions/Fetchboost.cmake
@@ -1,4 +1,4 @@
 include(findpathirdparty)
 
-set(boost_VERSION gcc8-1.58.0-bid5 CACHE INTERNAL "boost version")
+set(boost_VERSION gcc8-1.73.0-bid6 CACHE INTERNAL "boost version")
 findpathirdparty(boost boost ${boost_VERSION} ${CMAKE_SYSTEM_PROCESSOR})
