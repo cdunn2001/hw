912d7968edf75acc1d68b6bc6b598ff36a930ace
Ben Byington <bbyington@pacificbiosciences.com>
2022-02-14 15:27:47 -0800

    PTSD-404 upgraded to newer third party deps for pa-cal
    The primary driver was to get a newer pa-common, which has the
    file format we need for writing dark cal files.  That required bumping
    a bunch of versions for transitive dependancies, and that was comprehensive
    enough that I cleaned/upgraded the full set

--- src/pa-cal/nexus-versions/Fetchzeromq.cmake
+++ src/pa-cal/nexus-versions/Fetchzeromq.cmake
@@ -1,4 +1,4 @@
 include(findpathirdparty)
 
-set(zmq_VERSION 3.2.6-bid3 CACHE INTERNAL "zmq version")
+set(zmq_VERSION gcc8-3.2.6-bid4 CACHE INTERNAL "zmq version")
 findpathirdparty(zeromq zmq ${zmq_VERSION} ${CMAKE_SYSTEM_PROCESSOR})
