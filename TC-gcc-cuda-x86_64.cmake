77e16d575ce8e696bc54e5a3efea1c3c172aeaec
Curtis Gehman <cgehman@pacificbiosciences.com>
2022-02-24 02:09:32 +0000

    PTSD-1135: Add DebugCpu build type.

--- TC-gcc-cuda-x86_64.cmake
+++ TC-gcc-cuda-x86_64.cmake
@@ -12,6 +12,7 @@ set(CMAKE_CXX_FLAGS "-fdiagnostics-color=always -Wall -msse4.2 -Wno-missing-fiel
  CACHE STRING "" FORCE)
 
 set(CMAKE_CXX_FLAGS_DEBUG           "-O0 -g" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUGCPU        "-O0 -g" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE         "-O3 -DNDEBUG "    CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O3 -g " CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_ASAN            "-O0 -g -fsanitize=address -fno-omit-frame-pointer" CACHE STRING "" FORCE)
@@ -19,8 +20,9 @@ set(CMAKE_CXX_FLAGS_ASAN            "-O0 -g -fsanitize=address -fno-omit-frame-p
 set(CMAKE_CUDA_FLAGS                "--expt-relaxed-constexpr --default-stream per-thread -t 0\
                                      --compiler-options=\"${CMAKE_CXX_FLAGS}\""
                                     CACHE STRING "" FORCE)
-set(CMAKE_CUDA_FLAGS_RELEASE        "-O3 -DNDEBUG -lineinfo" CACHE STRING "" FORCE)
 set(CMAKE_CUDA_FLAGS_DEBUG          "-O0 -g -G" CACHE STRING "" FORCE)
+set(CMAKE_CUDA_FLAGS_DEBUGCPU       "-O0 -g -lineinfo" CACHE STRING "" FORCE)
+set(CMAKE_CUDA_FLAGS_RELEASE        "-O3 -DNDEBUG -lineinfo" CACHE STRING "" FORCE)
 set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-O3 -g -lineinfo" CACHE STRING "" FORCE)
 set(CMAKE_CUDA_FLAGS_ASAN           "${CMAKE_CUDA_FLAGS_DEBUG}" CACHE STRING "" FORCE)
 
