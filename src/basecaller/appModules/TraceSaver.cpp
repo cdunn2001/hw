d4409e47d47b61ea0e591af8f2a63432ecd48c65
Ben Byington <bbyington@pacificbiosciences.com>
2022-04-25 10:11:41 -0700

    PTSD-1434 Cleaned up some verbiage.  "Samples" is a better word to use than "UnitCell"

--- src/basecaller/appModules/TraceSaver.cpp
+++ src/basecaller/appModules/TraceSaver.cpp
@@ -79,7 +79,7 @@ TraceSaverBody::TraceSaverBody(const std::string& filename,
         enableWriterThread_ = true;
         writeFuture_ = std::async(std::launch::async, [this]()
         {
-            uint64_t unitCellsWritten = 0;
+            uint64_t samplesWritten = 0;
             PreppedTracesVariant data;
             while (enableWriterThread_)
             {
@@ -89,11 +89,11 @@ TraceSaverBody::TraceSaverBody(const std::string& filename,
                     {
                         using T = std::remove_pointer_t<decltype(traces->data())>;
                         file_.Traces().WriteTraceBlock<T>(*traces);
-                        unitCellsWritten += traces->num_elements();
+                        samplesWritten += traces->num_elements();
                     }, data);
                 }
             }
-            return unitCellsWritten;
+            return samplesWritten;
         });
     };
 
@@ -237,9 +237,9 @@ void TraceSaverBody::Process(PreppedTracesVariant traceVariant)
                             PBLOG_ERROR << "Trace Saving queue is full and the dedicated thread has died";
                             PBLOG_ERROR << "Checking for exception...";
                             assert(writeFuture_.valid());
-                            auto writtenPixels = writeFuture_.get();
+                            auto writtenSamples = writeFuture_.get();
                             PBLOG_ERROR << "No Exception found.  Thread terminated early after writing "
-                                        << writtenPixels << " unitCells";
+                                        << writtenSamples << " samples";
                             throw PBException("Unknown failure in TraceSaver");
                         }
                         PBLOG_WARN << "Trace Saving queue is full... Sleeping!";
@@ -272,11 +272,11 @@ TraceSaverBody::~TraceSaverBody()
         {
             PBLOG_INFO << "Waiting for SaverThread to complete";
             assert(writeFuture_.valid());
-            auto unitCellsWritten = writeFuture_.get();
-            if (unitCellsWritten == numFrames_ * numZmw_)
+            auto samplesWritten = writeFuture_.get();
+            if (samplesWritten == numFrames_ * numZmw_)
                 PBLOG_INFO << "SaverThread done, TraceFile is complete";
             else
-                PBLOG_WARN << "SaverThread finished without error, but only wrote " << unitCellsWritten << " unitCells out of " << numFrames_ * numZmw_;
+                PBLOG_WARN << "SaverThread finished without error, but only wrote " << samplesWritten << " samples out of " << numFrames_ * numZmw_;
         }
         catch (const std::exception& e)
         {
